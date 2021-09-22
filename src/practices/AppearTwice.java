9400b0ac8c72f3a4fb81aa811eb57c9b46f6af81
Ali Can <ali.can@finspiretech.com>
2021-09-22 14:20:53 +0100

    First commit

---
+++ src/practices/AppearTwice.java
@@ -0,0 +1,34 @@
+package practices;
+
+import java.util.Arrays;
+
+public class AppearTwice {
+
+    public static void main(String[] args) {
+        String sentence = "Hello Hello world!";
+        String trg = "Hello";
+        System.out.println(appearsTwice(trg, sentence));
+    }
+
+	public static boolean appearsTwice(String target, String sentence) {
+        boolean b = false;
+        int count = 0;
+        String [] str = sentence.split(" ");
+        System.out.println(Arrays.toString(str));
+        for(int i = 0; i<str.length; i++) {
+            if(str[i].toLowerCase().contains(target.toLowerCase())) {
+                count++;
+
+            }
+            if(count==2) {
+                b=true;
+            }else {
+
+             b= false;
+            }
+        }
+
+        return b;
+
+    }
+}
---
+++ src/practices/ReverseStr.java
@@ -0,0 +1,17 @@
+package practices;
+
+public class ReverseStr {
+    public static void main(String[] args) {
+        System.out.println(reverse("school"));
+    }
+    public static String reverse(String input)
+    {
+        String reversed= "";
+        for(int i = input.length()-1; i>=0; i--){
+            reversed += input.charAt(i)+"";
+
+        }
+        return reversed;
+
+    }
+}
